d85cb97f70b9822204bb2d67b4b0201f826d5844
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 10:24:30 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
